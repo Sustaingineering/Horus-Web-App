3cabede14614264a34abf2e6d8e332165245843f
Juan Luis Flores <jluisfgarza@gmail.com>
2018-10-01 14:22:32 -0700

    added tabs to dashboard layout

--- src/Content/Dashboard/Dashboard.jsx
+++ src/Content/Dashboard/Dashboard.jsx
@@ -5,6 +5,8 @@ import {
   Grid,
   Paper,
   withStyles,
+  Tabs,
+  Tab,
   Typography,
   MuiThemeProvider
 } from "@material-ui/core";
@@ -50,9 +52,26 @@ import {
 } from "./data";
 import { mainTheme } from "../../assets/jss/mainStyle";
 
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
 class Dashboard extends Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
     const { classes } = this.props;
+    const { value } = this.state;
 
     return (
       <Fragment>
@@ -63,117 +82,137 @@ class Dashboard extends Component {
             </Typography>
             <MonitoringData />
             <br />
-            <Grid container spacing={24}>
-              <Grid item xs={12} sm={12} md={6}>
-                <Paper className={classes.paper}>
-                  <ResponsiveContainer width="100%" height={300}>
-                    <AreaChart
-                      data={data2}
-                      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-                    >
-                      <CartesianGrid
-                        strokeDasharray="5 5"
-                        verticalFill={["263148", "#444444"]}
-                        fillOpacity={0.2}
-                      />
-                      <defs>
-                        <linearGradient
-                          id="colorUv"
-                          x1="0"
-                          y1="0"
-                          x2="0"
-                          y2="1"
+            <Tabs
+              value={value}
+              className={classes.tabs}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+              // scrollable
+              // scrollButtons="auto"
+            >
+              <Tab className={classes.tab} label="Real-Time" />
+              <Tab className={classes.tab} label="History" />
+              <Tab className={classes.tab} label="Summary" />
+            </Tabs>
+            {value === 0 && (
+              <TabContainer>
+                <Grid container spacing={24}>
+                  <Grid item xs={12} sm={12} md={6}>
+                    <Paper className={classes.paper}>
+                      <ResponsiveContainer width="100%" height={300}>
+                        <AreaChart
+                          data={data2}
+                          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
                         >
-                          <stop
-                            offset="5%"
-                            stopColor="#1d8cf8"
-                            stopOpacity={0.8}
+                          <CartesianGrid
+                            strokeDasharray="5 5"
+                            verticalFill={["263148", "#444444"]}
+                            fillOpacity={0.2}
                           />
-                          <stop
-                            offset="95%"
-                            stopColor="#1d8cf8"
-                            stopOpacity={0}
+                          <defs>
+                            <linearGradient
+                              id="colorUv"
+                              x1="0"
+                              y1="0"
+                              x2="0"
+                              y2="1"
+                            >
+                              <stop
+                                offset="5%"
+                                stopColor="#1d8cf8"
+                                stopOpacity={0.8}
+                              />
+                              <stop
+                                offset="95%"
+                                stopColor="#1d8cf8"
+                                stopOpacity={0}
+                              />
+                            </linearGradient>
+                            <linearGradient
+                              id="colorPv"
+                              x1="0"
+                              y1="0"
+                              x2="0"
+                              y2="1"
+                            >
+                              <stop
+                                offset="5%"
+                                stopColor="#00f2c3"
+                                stopOpacity={0.8}
+                              />
+                              <stop
+                                offset="95%"
+                                stopColor="#00f2c3"
+                                stopOpacity={0}
+                              />
+                            </linearGradient>
+                            <linearGradient
+                              id="colorAmt"
+                              x1="0"
+                              y1="0"
+                              x2="0"
+                              y2="1"
+                            >
+                              <stop
+                                offset="5%"
+                                stopColor="#fd5d93"
+                                stopOpacity={0.8}
+                              />
+                              <stop
+                                offset="95%"
+                                stopColor="#fd5d93"
+                                stopOpacity={0}
+                              />
+                            </linearGradient>
+                          </defs>
+                          <XAxis
+                            dataKey="name"
+                            tick={{ fill: "white" }}
+                            stroke="#efefef"
                           />
-                        </linearGradient>
-                        <linearGradient
-                          id="colorPv"
-                          x1="0"
-                          y1="0"
-                          x2="0"
-                          y2="1"
-                        >
-                          <stop
-                            offset="5%"
-                            stopColor="#00f2c3"
-                            stopOpacity={0.8}
+                          <YAxis tick={{ fill: "white" }} stroke="#efefef" />
+                          <Tooltip />
+                          <Area
+                            type="monotone"
+                            dataKey="uv"
+                            stackId="1"
+                            stroke="#1d8cf8"
+                            strokeWidth="3"
+                            fill="#1d8cf8"
+                            fillOpacity={1}
+                            fill="url(#colorUv)"
                           />
-                          <stop
-                            offset="95%"
-                            stopColor="#00f2c3"
-                            stopOpacity={0}
+                          <Area
+                            type="monotone"
+                            dataKey="pv"
+                            stackId="1"
+                            stroke="#00f2c3"
+                            strokeWidth="3"
+                            fill="#00f2c3"
+                            fillOpacity={1}
+                            fill="url(#colorPv)"
                           />
-                        </linearGradient>
-                        <linearGradient
-                          id="colorAmt"
-                          x1="0"
-                          y1="0"
-                          x2="0"
-                          y2="1"
-                        >
-                          <stop
-                            offset="5%"
-                            stopColor="#fd5d93"
-                            stopOpacity={0.8}
+                          <Area
+                            type="monotone"
+                            dataKey="amt"
+                            stackId="1"
+                            stroke="#fd5d93"
+                            strokeWidth="3"
+                            fill="#fd5d93"
+                            fillOpacity={1}
+                            fill="url(#colorAmt)"
                           />
-                          <stop
-                            offset="95%"
-                            stopColor="#fd5d93"
-                            stopOpacity={0}
-                          />
-                        </linearGradient>
-                      </defs>
-                      <XAxis
-                        dataKey="name"
-                        tick={{ fill: "white" }}
-                        stroke="#efefef"
-                      />
-                      <YAxis tick={{ fill: "white" }} stroke="#efefef" />
-                      <Tooltip />
-                      <Area
-                        type="monotone"
-                        dataKey="uv"
-                        stackId="1"
-                        stroke="#1d8cf8"
-                        strokeWidth="3"
-                        fill="#1d8cf8"
-                        fillOpacity={1}
-                        fill="url(#colorUv)"
-                      />
-                      <Area
-                        type="monotone"
-                        dataKey="pv"
-                        stackId="1"
-                        stroke="#00f2c3"
-                        strokeWidth="3"
-                        fill="#00f2c3"
-                        fillOpacity={1}
-                        fill="url(#colorPv)"
-                      />
-                      <Area
-                        type="monotone"
-                        dataKey="amt"
-                        stackId="1"
-                        stroke="#fd5d93"
-                        strokeWidth="3"
-                        fill="#fd5d93"
-                        fillOpacity={1}
-                        fill="url(#colorAmt)"
-                      />
-                    </AreaChart>
-                  </ResponsiveContainer>
-                </Paper>
-              </Grid>
-            </Grid>
+                        </AreaChart>
+                      </ResponsiveContainer>
+                    </Paper>
+                  </Grid>
+                </Grid>
+              </TabContainer>
+            )}
+            {value === 1 && <TabContainer>History</TabContainer>}
+            {value === 2 && <TabContainer>Summary</TabContainer>}
           </div>
         </MuiThemeProvider>
       </Fragment>
