c3cb9a262fa78e70654d50ac80ee771bf12cc49a
john <likisongproduct@gmail.com>
2018-11-01 21:54:18 -0700

    first changes

--- src/Content/Dashboard/Chart.jsx
+++ src/Content/Dashboard/Chart.jsx
@@ -12,7 +12,8 @@ import {
   ResponsiveContainer,
   CartesianGrid,
   AreaChart,
-  Area
+  Area,
+  Label
 } from "recharts";
 
 class Chart extends React.Component {
@@ -20,17 +21,31 @@ class Chart extends React.Component {
     const { classes } = this.props;
     return (
       <Paper className={classes.paper}>
-        <div className={classes.title}>{this.props.title}</div>
+        {/* <div className={classes.title}>{this.props.title}</div> */}
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart
             data={this.props.data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            margin={{ top: 30, right: 30, left: 0, bottom: 12 }}
           >
             <CartesianGrid
               strokeDasharray="5 5"
               verticalFill={["263148", "#444444"]}
               fillOpacity={0.2}
             />
+
+        
+
+            {/* <YAxis >
+              <Label value={this.props.yaxis}/>
+            </YAxis> */}
+            <YAxis label={{value: this.props.yaxis, fill: 'white', angle: -90, position: 'insideLeft' }} />
+            {/* <XAxis label={{ value: 'pv of page', fill: 'white', position: 'insideBottom', dy: 10}} /> */}
+            <XAxis dataKey="name">
+              <Label value="Time" offset={0} fill= 'white' position="insideBottom" dy={10}/>
+
+              <Label value={this.props.title} offset={0} fontSize={20} fill= 'white' position="top" dy={-240} />
+            </XAxis>
+            
             <defs>
               <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="5%" stopColor="#1d8cf8" stopOpacity={0.8} />
