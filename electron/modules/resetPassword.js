86c51dce669aa7e003adc5a2e22534786dca0626
Felipe <felipeballesteros1@gmail.com>
2018-11-16 13:39:57 -0800

    Added test framework, refactored reset password

--- electron/modules/resetPassword.js
+++ electron/modules/resetPassword.js
@@ -18,67 +18,61 @@ const emailTemplate = {
 };
 
 //Verify email
-var generatePasswordToken = exports.generatePasswordToken =  async (e, msg) => {
-  try {
-    console.log('inside pass')
-    // Check email existance
-    let emailExists = await datastore.findUser(msg.email);
-    console.log('inside pass22', emailExists)
-
-    if (!emailExists) {
-      e.sender.send('email-exists', {error:"Email does not exist"});
-      return;
-    }
-    console.log('inside pass2')
-
-    let userEmail = msg.email;
-    //Generate random code to recover password
-    let randomCode = Math.random().toString(36).substring(7);
-    //Populate email template
-    emailTemplate.to = userEmail;
-    emailTemplate.html =
-    `
-    <p>
-    Hi,
-      This is your temporary verification code: ` + randomCode + `
-    </p>
-    `
-    console.log('inside pass4')
-
-    //Create Password token on Database
-    await datastore.storePasswordToken(randomCode, userEmail)
-    console.log('inside pass5')
-
-    //Send email to user
-    await transporter.sendMail(mailOptions, function (error, info) {
-      if(error) {
-        console.log(error);
-      } else {
-        console.log(info);
+var generatePasswordToken = exports.generatePasswordToken = (msg) => {
+  return new Promise( async (resolve, reject) => {
+    try {
+      // Check email existance
+      let emailExists = await datastore.findUser(msg.email);
+      if (!emailExists) {
+        return resolve({error:"Email does not exist"});
       }
-    });
-    e.sender.send('email-exists', "Email exists!");
-  } catch(error) {
-    console.log(error)
-    e.sender.send('email-exists', {error: error}); 
-  }
+      let userEmail = msg.email;
+      //Generate random code to recover password
+      let randomCode = Math.random().toString(36).substring(7);
+      //Populate email template
+      emailTemplate.to = userEmail;
+      emailTemplate.html =
+      `
+      <p>
+      Hi,
+        This is your temporary verification code: ` + randomCode + `
+      </p>
+      `
+      //Create Password token on Database
+      await datastore.storePasswordToken(randomCode, userEmail)
+      //Send email to user
+      await transporter.sendMail(emailTemplate, function (error, info) {
+        if(error) {
+          console.log(error);
+        } else {
+          console.log(info);
+        }
+      });
+      return resolve({success:"Verification Email sent"});
+    } catch(error) {
+      console.log(error)
+      return reject(error) 
+    }
+  })
 }
 
-//ipcMain.on('generate-password-token', generatePasswordToken)
 
   //VerifyCode
-// ipcMain.on('verify-and-update-password', async (e, msg) => {
-//   try {
-//     let isTokenValid = await datastore.checkPasswordToken(msg.token, msg.email);
-//     if(!isTokenValid) {
-//       e.sender.send('verify-code', {error: 'Wrong Verification Code entered'});
-//       return;
-//     }
-//     //Reset passoword
-//     await datastore.newPassword({email: msg.email, password: msg.password});
-//     await datastore.clearPasswordTokens(msg.email);
-//     e.sender.send('reset-password', "Password changed!");
-//   } catch(error) {
-//     e.sender.send('verify-code', {error: error});
-//   }
-// }) 
+
+var verifyAndUpdatePassword = exports.verifyAndUpdatePassword = function(msg) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let isTokenValid = await datastore.checkPasswordToken(msg.token, msg.email);
+      if(!isTokenValid) {
+        return resolve({error: 'Wrong Verification Code entered'});
+      }
+      //Reset passoword
+      await datastore.newPassword({email: msg.email, password: msg.password});
+      await datastore.clearPasswordTokens(msg.email);
+      return resolve({success:"Password changed!"});
+    } catch(error) {
+      return reject({error: error});  
+    }
+  })
+}
+ 
