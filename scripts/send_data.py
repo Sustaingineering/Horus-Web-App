ceae0197aefc3e0d8456f3cafaa048570398e7cb
Leslie Cheng <3242609+lcfyi@users.noreply.github.com>
2019-08-03 14:39:38 -0700

    Updated example to include using timestamp as key.

--- scripts/send_data.py
+++ scripts/send_data.py
@@ -1,5 +1,6 @@
 import pyrebase
 import json
+import math
 import random
 from time import sleep as s
 from time import time as t
@@ -13,17 +14,22 @@ c = {
 
 fb = pyrebase.initialize_app(c)
 db = fb.database()
+i = 0
+sensors = ["123456", "69420", "1"]
 
 while True:
     shape = {
         "power": random.randrange(200, 600),
         "surface-temperature": random.randrange(10, 30),
         "op-temp": random.randrange(10, 30),
-        "current": random.randrange(0, 5),
+        "current": math.sin(i),
         "water-breaker": random.randrange(0, 3),
         "time-stamp": int(t()),
-        "voltage": random.randrange(0, 10)
+        "voltage": math.cos(i)
     }
-    db.child("69420").push(shape)
+    i += 0.1
+    for sensor in sensors:
+        db.child(sensor).update({
+            str(int(t())) : shape
+        })
     s(2)
-
