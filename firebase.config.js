8c900dd1f585cf128ebc0913cde62a255929f6ca
lcfyi <3242609+lcfyi@users.noreply.github.com>
2020-02-03 18:10:05 -0800

    Programmatically set firebase configuration for database, firestore, and hosting.

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBI6chLsGI6XX67LGTXXH_gXLfngCS3aNs",
+  authDomain: "sustaingineering-horus.firebaseapp.com",
+  databaseURL: "https://sustaingineering-horus.firebaseio.com",
+  projectId: "sustaingineering-horus",
+  storageBucket: "sustaingineering-horus.appspot.com",
+  messagingSenderId: "717859757619",
+  appId: "1:717859757619:web:c59688eb51d2fec3"
+};
+
+export default firebaseConfig;
--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,17 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 import React from 'react';
-
-// Unique, but non-secret firebase context details
-var firebaseConfig = {
-  apiKey: "AIzaSyBI6chLsGI6XX67LGTXXH_gXLfngCS3aNs",
-  authDomain: "sustaingineering-horus.firebaseapp.com",
-  databaseURL: "https://sustaingineering-horus.firebaseio.com",
-  projectId: "sustaingineering-horus",
-  storageBucket: "sustaingineering-horus.appspot.com",
-  messagingSenderId: "717859757619",
-  appId: "1:717859757619:web:c59688eb51d2fec3"
-};
+import firebaseConfig from "../../firebase.config.js";
 
 // Default export of firebase instance
 export default class Firebase {
