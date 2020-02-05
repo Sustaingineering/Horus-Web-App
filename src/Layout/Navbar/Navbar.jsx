488f4d61c0a18eb4e6249d0056894a193e65d437
lcfyi <3242609+lcfyi@users.noreply.github.com>
2020-02-04 21:40:01 -0800

    Remove unnecessary fragment.

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment } from "react";
+import React, { PureComponent } from "react";
 // Material UI Components
 import { AppBar, Toolbar, IconButton, Button } from "@material-ui/core";
 // Navbar Menu - drawer component
@@ -33,48 +33,46 @@ class Navbar extends PureComponent {
     this.props.firebase.auth().currentUser.reload();
 
     return (
-      <Fragment>
-        <div className={classes.root}>
-          <AppBar
-            position="static"
-            className={classNames(
-              classes.appBar,
-              this.state.open && classes.appBarShift
-            )}
-          >
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={() => {
-                  this.setDrawerOpenState(true);
-                }}
-                className={classNames(
-                  classes.menuButton,
-                  this.state.open && classes.hide
-                )}
-              >
-                <MenuIcon />
-              </IconButton>
-              <div className={classes.flex}></div>
-              <Button
-                className={classes.button}
-                color="primary"
-                onClick={this.signOut}
-              >
-                Sign Out
-              </Button>
-            </Toolbar>
-          </AppBar>
-          <NavBarMenu
-            firebase={this.props.firebase}
-            updateSensors={this.props.updateSensors}
-            sensors={this.props.sensors}
-            isOpen={this.state.open}
-            changeDrawerOpen={this.setDrawerOpenState}
-          />
-        </div>
-      </Fragment>
+      <div className={classes.root}>
+        <AppBar
+          position="static"
+          className={classNames(
+            classes.appBar,
+            this.state.open && classes.appBarShift
+          )}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={() => {
+                this.setDrawerOpenState(true);
+              }}
+              className={classNames(
+                classes.menuButton,
+                this.state.open && classes.hide
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            <div className={classes.flex}></div>
+            <Button
+              className={classes.button}
+              color="primary"
+              onClick={this.signOut}
+            >
+              Sign Out
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <NavBarMenu
+          firebase={this.props.firebase}
+          updateSensors={this.props.updateSensors}
+          sensors={this.props.sensors}
+          isOpen={this.state.open}
+          changeDrawerOpen={this.setDrawerOpenState}
+        />
+      </div>
     );
   }
 }
