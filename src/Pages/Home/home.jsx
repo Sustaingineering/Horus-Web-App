939161bc48568a90bc0d6e6bf3410aa4f2d6bb1e
lcfyi <3242609+lcfyi@users.noreply.github.com>
2020-02-04 19:13:19 -0800

    Home refactors.

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,10 +1,9 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { Fragment, PureComponent } from "react";
 import { Link } from "react-router-dom";
-import { withStyles } from '@material-ui/core/styles';
-import { Paper, Typography, Grid, Card, CardContent } from '@material-ui/core';
-import styles from './homeStyle';
-// Logos
-import logos from '../../assets/images/logos.png';
+import { withStyles } from "@material-ui/core/styles";
+import { Paper, Typography, Grid, Card, CardContent } from "@material-ui/core";
+import logos from "../../assets/images/logos.png";
+import styles from "./homeStyle";
 
 class Home extends PureComponent {
   render() {
@@ -18,48 +17,59 @@ class Home extends PureComponent {
               <Grid item md={6}>
                 <div className={classes.mainFeaturedPostContent}>
                   <Typography
-                    component='h1'
-                    variant='h4'
-                    color='inherit'
+                    component="h1"
+                    variant="h4"
+                    color="inherit"
                     gutterBottom
                   >
                     {user === null ? "Welcome!" : "Welcome, " + user + "!"}
                   </Typography>
-                  <Typography variant='subtitle1' color='inherit' paragraph>
+                  <Typography variant="subtitle1" color="inherit" paragraph>
                     Sustaingineering is a student engineering design team that
-                    designs, develops and deploys sustainable technology solutions
-                    for renewable energy applications in remote and developing
-                    communities. Our goal is to create power solutions to address
-                    the global challenge of climate change and to improve the
-                    quality of life of the people living in these communities.
+                    designs, develops and deploys sustainable technology
+                    solutions for renewable energy applications in remote and
+                    developing communities. Our goal is to create power
+                    solutions to address the global challenge of climate change
+                    and to improve the quality of life of the people living in
+                    these communities.
                   </Typography>
                 </div>
               </Grid>
             </Grid>
           </Paper>
           <Grid container spacing={40} className={classes.cardGrid}>
-            {user === null ?
+            {user === null ? (
               <Grid item key={"no-user-auth"} xs={12} md={6}>
-              <Card className={classes.card}>
-                <div className={classes.cardDetails}>
-                  <CardContent>
-                    <h2 className={classes.postTitle}>Set your name!</h2>
-                    <div 
-                      className={classes.postText} 
-                      variant='subtitle1' 
-                      color='inherit' 
-                    >Visit <Link to="/config">config</Link> to set your name.</div>
-                  </CardContent>
-                </div>
-              </Card>
-            </Grid> : undefined}
+                <Card className={classes.card}>
+                  <div className={classes.cardDetails}>
+                    <CardContent>
+                      <h2 className={classes.postTitle}>Set your name!</h2>
+                      <div
+                        className={classes.postText}
+                        variant="subtitle1"
+                        color="inherit"
+                      >
+                        Visit <Link to="/config">config</Link> to set your name.
+                      </div>
+                    </CardContent>
+                  </div>
+                </Card>
+              </Grid>
+            ) : (
+              undefined
+            )}
             {this.props.posts.map(post => (
               <Grid item key={post.title} xs={12} md={6}>
                 <Card className={classes.card}>
                   <div className={classes.cardDetails}>
                     <CardContent>
                       <h2 className={classes.postTitle}>{post.title}</h2>
-                      <div className={classes.postText} variant='subtitle1' color='inherit' dangerouslySetInnerHTML={{__html: post.text}}></div>
+                      <div
+                        className={classes.postText}
+                        variant="subtitle1"
+                        color="inherit"
+                        dangerouslySetInnerHTML={{ __html: post.text }}
+                      ></div>
                     </CardContent>
                   </div>
                 </Card>
@@ -67,11 +77,11 @@ class Home extends PureComponent {
             ))}
           </Grid>
         </div>
-        <footer className={classes.footer}>
-          <img className={classes.logos} src={logos} alt='logos' />
+        <footer>
+          <img className={classes.logos} src={logos} alt="logos" />
         </footer>
       </Fragment>
-    )
+    );
   }
 }
 
--- src/Pages/Home/homeStyle.jsx
+++ src/Pages/Home/homeStyle.jsx
@@ -1,32 +1,32 @@
 // Styles
-import bg from '../../assets/images/bg.jpg';
+import bg from "../../assets/images/bg.jpg";
 
 const styles = theme => ({
   layout: {
-    width: 'auto',
+    width: "auto",
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     paddingTop: 50,
     paddingBottom: 50
   },
   mainFeaturedPost: {
-    backgroundImage: 'url(' + bg + ')',
-    backgroundColor: '#27293d',
+    backgroundImage: "url(" + bg + ")",
+    backgroundColor: "#27293d",
     color: theme.palette.common.white,
     marginBottom: theme.spacing.unit * 4
   },
   mainFeaturedPostContent: {
-    height: '100%',
-    width: 'auto',
+    height: "100%",
+    width: "auto",
     padding: `${theme.spacing.unit * 6}px`,
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up("md")]: {
       paddingRight: 0
     }
   },
   card: {
-    backgroundColor: '#27293d',
-    display: 'flex',
-    color: 'white'
+    backgroundColor: "#27293d",
+    display: "flex",
+    color: "white"
   },
   cardDetails: {
     flex: 1
@@ -37,7 +37,7 @@ const styles = theme => ({
   },
   postText: {
     margin: 15,
-    lineHeight: 1.5,
+    lineHeight: 1.5
   },
   cardMedia: {
     width: 160
@@ -48,24 +48,24 @@ const styles = theme => ({
   },
   footer: {},
   logos: {
-    marginTop: '2%',
-    marginLeft: 'calc(50% - 350px)',
+    marginTop: "2%",
+    marginLeft: "calc(50% - 350px)",
     width: 650,
-    height: 'auto',
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: 'calc(50% - 250px)',
+    height: "auto",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "calc(50% - 250px)",
       width: 450
     },
-    [theme.breakpoints.down('xs')]: {
-      display: 'none'
+    [theme.breakpoints.down("xs")]: {
+      display: "none"
     }
   },
   menuButton: {
-    position: 'relative',
+    position: "relative",
     marginLeft: 0,
     marginRight: 20,
     marginBottom: 20
   }
-})
+});
 
-export default styles
+export default styles;
