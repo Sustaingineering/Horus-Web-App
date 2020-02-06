3e6d3ec24d3cbd6b69783c4738defebb2ab3a30c
lcfyi <3242609+lcfyi@users.noreply.github.com>
2020-02-06 04:46:29 -0800

    Click to dismiss sidebar.

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import React, { PureComponent } from "react";
 // Material UI Components
-import { AppBar, Toolbar, IconButton, Button, Grid } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Grid,
+  ClickAwayListener
+} from "@material-ui/core";
 // Navbar Menu - drawer component
 import NavBarMenu from "../NavBarMenu/NavBarMenu";
 // Icons
@@ -41,44 +48,51 @@ class Navbar extends PureComponent {
     this.props.firebase.auth().currentUser.reload();
 
     return (
-      <div className={classes.root}>
-        <AppBar position="static" className={classes.appBar}>
-          <Toolbar>
-            <Grid justify="space-between" container spacing={0}>
-              <Grid item>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={() => {
-                    this.setDrawerOpenState(!this.state.drawerOpen);
-                    this.setSensorOpenState(false);
-                  }}
-                  className={classes.menuButton}
-                >
-                  <MenuIcon />
-                </IconButton>
+      <ClickAwayListener
+        onClickAway={() => {
+          this.setDrawerOpenState(false);
+          this.setSensorOpenState(false);
+        }}
+      >
+        <div className={classes.root}>
+          <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+              <Grid justify="space-between" container spacing={0}>
+                <Grid item>
+                  <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={() => {
+                      this.setDrawerOpenState(!this.state.drawerOpen);
+                      this.setSensorOpenState(false);
+                    }}
+                    className={classes.menuButton}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={classes.button}
+                    color="primary"
+                    onClick={this.signOut}
+                  >
+                    Sign Out
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Button
-                  className={classes.button}
-                  color="primary"
-                  onClick={this.signOut}
-                >
-                  Sign Out
-                </Button>
-              </Grid>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-        <NavBarMenu
-          firebase={this.props.firebase}
-          sensors={this.props.sensors}
-          isDrawerOpen={this.state.drawerOpen}
-          isSensorsOpen={this.state.sensorsOpen}
-          setDrawerOpenState={this.setDrawerOpenState}
-          setSensorOpenState={this.setSensorOpenState}
-        />
-      </div>
+            </Toolbar>
+          </AppBar>
+          <NavBarMenu
+            firebase={this.props.firebase}
+            sensors={this.props.sensors}
+            isDrawerOpen={this.state.drawerOpen}
+            isSensorsOpen={this.state.sensorsOpen}
+            setDrawerOpenState={this.setDrawerOpenState}
+            setSensorOpenState={this.setSensorOpenState}
+          />
+        </div>
+      </ClickAwayListener>
     );
   }
 }
