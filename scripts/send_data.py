ed321d1a1a0db9068355a97c9c515f634d26e209
Leslie Cheng <leslie.cheng5@gmail.com>
2020-02-07 02:11:30 -0800

    Update outdated library.

--- scripts/send_data.py
+++ scripts/send_data.py
@@ -1,36 +1,33 @@
-import pyrebase
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
 import json
 import math
 import random
 from time import sleep as s
 from time import time as t
 
-c = {
-  "apiKey": "AIzaSyBI6chLsGI6XX67LGTXXH_gXLfngCS3aNs",
-  "authDomain": "sustaingineering-horus.firebaseapp.com",
-  "databaseURL": "https://sustaingineering-horus.firebaseio.com",
-  "storageBucket": "sustaingineering-horus.appspot.com",
-  "serviceAccount": "serviceAccount.json"
-}
+cred = credentials.Certificate("serviceAccount.json")
+fb = firebase_admin.initialize_app(cred, options={
+    "databaseURL": "https://sustaingineering-horus.firebaseio.com"
+})
 
-fb = pyrebase.initialize_app(c)
-db = fb.database()
 i = 0
 sensors = ["123456", "69420", "1"]
 
 while True:
+    time = t()
     shape = {
         "power": random.randrange(200, 600),
         "surface-temperature": random.randrange(10, 30),
         "op-temp": random.randrange(10, 30),
         "current": math.sin(i),
         "water-breaker": random.randrange(0, 3),
-        "time-stamp": int(t()),
+        "time-stamp": int(time),
         "voltage": math.cos(i)
     }
     i += 0.1
     for sensor in sensors:
-        db.child(sensor).update({
-            str(int(t())) : shape
-        })
+        # Update or set is the same here, since we're accessing a child that's unique already
+        db.reference(path=sensor).child(str(int(time))).set(shape)
     s(2)
