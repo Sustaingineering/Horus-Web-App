eae20910dd4768ff25c72ce122bf6b68f94376e1
Fei <fei_fei_0316@hotmail.com>
2020-02-08 03:08:41 -0800

    Applied old code to updated master. WIP.

--- src/Pages/Dashboard/Sensor.jsx
+++ src/Pages/Dashboard/Sensor.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, {PureComponent, Fragment, useCallback} from "react";
 // Material UI Components
 import { withStyles } from "@material-ui/core";
 //Style
@@ -12,6 +12,14 @@ class Sensor extends PureComponent {
     this.state = {
       data: []
     };
+        let date = new Date();
+        let now = date.getTime();
+        let d = now - (365 * 24 * 60 * 60 * 1000);
+        this.timestamp = Math.floor(d / 1000);
+        this.nowstamp = Math.floor(now / 1000);
+        this.setState({range: this.timestamp});
+        this.setState({selected: 3});
+        //this.getDatabase();
   }
 
   // We wait until the component has been mounted (which means
@@ -21,11 +29,58 @@ class Sensor extends PureComponent {
     this.getDatabase(this.props.sensorId);
   };
 
+    changeRange = (value) => {
+        this.setState({selected: value});
+        console.log(value);
+        let offset;
+        if (value === 1) {
+            offset = 60 * 60 * 1000;
+        } else if (value === 2) {
+            offset = 2 * 60 * 60 * 1000;
+        } else if (value === 3) {
+            offset = 6 * 60 * 60 * 1000;
+        } else if (value === 4) {
+            offset = 24 * 60 * 60 * 1000;
+        } else if (value === 5) {
+            offset = 7 * 24 * 60 * 60 * 1000;
+        } else if (value === 6) {
+            offset = 30 * 24 * 60 * 60 * 1000;
+        } else if (value === 7) {
+            offset = 183 * 24 * 60 * 60 * 1000;
+        } else if (value === 8) {
+            offset = 365 * 24 * 60 * 60 * 1000;
+        } else {
+            offset = 6 * 60 * 60 * 1000;
+        }
+        let date = new Date();
+        let now = date.getTime();
+        let d = now - offset;
+        this.timestamp = Math.floor(d / 1000);
+        console.log(this.timestamp);
+        this.nowstamp = Math.floor(now / 1000);
+        console.log(this.nowstamp);
+        this.setState({range: this.timestamp});
+        this.getDatabase(this.props.sensorId);
+    }
+
   getDatabase = sensorId => {
+
+        console.log(this.timestamp);
+        console.log(this.nowstamp);
+
     this.db = this.props.firebase.database().ref(sensorId);
     let temp = [];
     // Use .once() to make it call less data
-    this.db.limitToLast(30).once("value", e => {
+        // this.db.limitToLast(30).once("value", e => {
+        //   for (let i in e.val()) {
+        //     temp.push(e.val()[i]);
+        //   }
+        // })
+
+        this.db.orderByKey()
+            .startAt(this.timestamp.toString())
+            .endAt(this.nowstamp.toString())
+            .once("value", e => {
       for (let i in e.val()) {
         temp.push(e.val()[i]);
       }
@@ -52,6 +107,8 @@ class Sensor extends PureComponent {
           data={this.state.data}
           sensorName={this.props.sensorName}
           sensorId={this.props.sensorId}
+          changeRange={this.changeRange}
+          selected={this.state.selected}
           firebase={this.props.firebase}
         />
       </div>
